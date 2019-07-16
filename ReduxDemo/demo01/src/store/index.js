904463be31924c3d5c748322301f7c265f280c2b
xiechengyu <2390936921@qq.com>
2019-07-16 14:05:09 +0800

    新增redux-thunk中间件

--- ReduxDemo/demo01/src/store/index.js
+++ ReduxDemo/demo01/src/store/index.js
@@ -1,7 +1,10 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from 'redux-thunk'
 import reducer from "./reducer";
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-); // 创建数据存储仓库);
+
+// 利用compose创造一个增强函数  相当于建立了一个链式函数
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose
+const enhancer = composeEnhancers(applyMiddleware(thunk))
+const store = createStore(reducer, enhancer); // 创建数据存储仓库);
+
 export default store;
