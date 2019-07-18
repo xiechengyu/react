9380fd42114b27e50a7217c27e7ea457b3d66578
xiechengyu <2390936921@qq.com>
2019-07-18 18:04:27 +0800

    继续开发header组件并引入redux

--- ReduxDemo/demo01/src/store/index.js
+++ ReduxDemo/demo01/src/store/index.js
@@ -1,10 +1,15 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import thunk from 'redux-thunk'
 import reducer from "./reducer";
+// import thunk from 'redux-thunk'
+import createSagaMiddleware from 'redux-saga'
+import mySagas from "./sagas"
+const sagaMiddleware = createSagaMiddleware();
 
 // 利用compose创造一个增强函数  相当于建立了一个链式函数
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose
-const enhancer = composeEnhancers(applyMiddleware(thunk))
+// const enhancer = composeEnhancers(applyMiddleware(thunk))
+const enhancer = composeEnhancers(applyMiddleware(sagaMiddleware))
 const store = createStore(reducer, enhancer); // 创建数据存储仓库);
+sagaMiddleware.run(mySagas)
 
 export default store;
