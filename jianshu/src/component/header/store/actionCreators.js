53b66a1471d79a1eb9e71ef2e7a3a74459d7479a
xiechengyu <2390936921@qq.com>
2019-07-18 23:44:01 +0800

    header组件跟进开发

---
+++ jianshu/src/component/header/store/actionCreators.js
@@ -0,0 +1,8 @@
+import {INPUT_FOCUS,INPUT_BLUR} from '../../../store/actionTypes'
+
+export const inputFocus = () => ({
+    type:INPUT_FOCUS
+})
+export const inputBlur = () => ({
+    type:INPUT_BLUR
+})
--- jianshu/src/component/header/store/index.js
+++ jianshu/src/component/header/store/index.js
@@ -1,9 +1,5 @@
-import {createStore,applyMiddleware,compose} from 'redux'
-import thunk from 'redux-thunk'
-import reducer from './reducer';
+import reducer from './reducer'
+import * as actionCreators from './actionCreators'
+import * as constants from './actionTypes'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__?window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}):compose
-const enhancer = composeEnhancers(applyMiddleware(thunk))
-const store = createStore(reducer,enhancer)
-
-export default store
+export{reducer,actionCreators,constants}
